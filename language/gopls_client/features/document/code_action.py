1af4588542e53b0b31390365fca9096c6d04e13b
ginanjarn <ginanjarn@gmail.com>
2025-08-30 22:32:22 +0700

    update plugin_core commit add fix error

--- language/gopls_client/features/document/code_action.py
+++ language/gopls_client/features/document/code_action.py
@@ -1,6 +1,3 @@
-from collections import namedtuple
-from functools import wraps
-
 from ....plugin_core.session import Session
 from ....plugin_core.features.document.code_action import (
     DocumentCodeActionMixins,
@@ -9,37 +6,14 @@ from ....plugin_core.features.document.code_action import (
 from ....plugin_core.features.workspace.edit import WorkspaceEdit
 
 
-LineCharacter = namedtuple("LineCharacter", ["line", "character"])
-
-
-def must_initialized(func):
-    """exec if initialized"""
-
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        if not self.session.is_initialized():
-            return None
-        return func(self, *args, **kwargs)
-
-    return wrapper
-
-
-class GoplsCodeActionResolveMixins(CodeActionResolveMixins):
-
-    def _handle_action(self, session: Session, action: dict) -> None:
-        if edit := action.get("edit"):
-            WorkspaceEdit(session).apply_changes(edit)
-        if command := action.get("command"):
-            self.workspace_executecommand(command)
-
-
 class GoplsDocumentCodeActionMixins(
     DocumentCodeActionMixins,
-    GoplsCodeActionResolveMixins,
+    CodeActionResolveMixins,
 ):
+    """"""
 
     def _handle_selected_action(self, session: Session, action: dict) -> None:
         if edit := action.get("edit"):
             WorkspaceEdit(session).apply_changes(edit)
         if _ := action.get("command"):
-            self.code_action_resolve(action)
+            self.codeaction_resolve(action)
